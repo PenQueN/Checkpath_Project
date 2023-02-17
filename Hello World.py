3076e855d66faf514297a5c550776da0d1a08db7
PenQueN <49773582+PenQueN@users.noreply.github.com>
2023-02-17 07:18:40 +0700

    Upload File & Folder

---
+++ Hello World.py
@@ -0,0 +1,15 @@
+#พิมพ์ Hello World
+print('============')
+print('Hello Uncle')
+print('Hello Somchai')
+print('Hello Github')
+print('============')
+
+#พิมพ์ Hello World 
+print('Hello BoY \n' *3)
+
+friend = ['Loong','Panda','PenQuin']
+
+print(friend[0])
+
+
